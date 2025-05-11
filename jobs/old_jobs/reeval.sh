d6de09dcae235bdc7dff4908a67d987bb50a4075
HiFei4869 <christine.yifei.song@outlook.com>
2025-05-11 14:32:30 +0800

    update

--- jobs/old_jobs/reeval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in $(find "/storage/brno2/home/bronecja/llm_thesis_logs" -maxdepth 1)
-do
-   if [ -d $f'/model' ]; then
-      qsub -q gpu -v RUNLOGDIR="'$f'" just_eval.sh
-   fi
-done
--- jobs/spawn_1B_evals.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for rundir in $(ls /storage/brno2/home/bronecja/1B_unlearning_logs/); do
-    rundir="/storage/brno2/home/bronecja/1B_unlearning_logs/$rundir"
-    qsub -q gpu -v RUNLOGDIR="'$rundir'" eval_1Bs.sh
-done
